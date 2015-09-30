628c1eb8073f499b706a5fc217842c5941038372
gSchool Student <gSchool@Datas-Mac-mini-14.local>
2015-09-30 12:08:17 -0700

    Update scratch file.

--- code/scratch.py
+++ code/scratch.py
@@ -1,125 +1,64 @@
+
+# coding: utf-8
+
+# In[31]:
+
 import pandas as pd
-
-with open('amicus/debug_files/ok_case_names') as f:
-    case_text = f.readlines()
-approved_cases = [case.split('.')[0] for case in case_text[7:]]
-
-df = pd.read_csv('amicus/scdb/SCDB_2015_01_caseCentered_Citation.csv')
-print '\n'.join(df.columns)
-
-
-# In the SCDB the docket column is called 'docket'
-columns_of_interest = ['docket',\
-                       'dateArgument',\
-                       'caseIssuesId',\
-                       'certReason',\
-                       'naturalCourt',\
-                       'chief',\
-                       'issueArea',\
-                       'jurisdiction',\
-                       'partyWinning']
-
-df_subset = df[df['docket'].isin(approved_cases)][columns_of_interest]
-
-test = df_subset[df_subset.isnull().any(axis=1)]
-df_final = df_subset[df_subset.notnull().all(axis=1)]
-
-df_final['argument_month'] = df_final['dateArgument'].apply(lambda d: int(d.split('/')[0]))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 
-from sklearn.cross_validation import cross_val_score
-
 from sklearn.linear_model import LogisticRegression
 from sklearn.ensemble import RandomForestClassifier
-from sklearn.metrics import accuracy_score,                             precision_score,                             recall_score,                             classification_report,                             confusion_matrix
-                
 
+from sklearn.cross_validation import cross_val_score
+from sklearn.metrics import accuracy_score,                            precision_score,                            recall_score,                            classification_report,                            confusion_matrix                
 get_ipython().magic(u'matplotlib inline')
+
+
+# In[32]:
 
 with open('amicus/debug_files/ok_case_names') as f:
     case_text = f.readlines()
 approved_cases = [case.split('.')[0] for case in case_text[7:]]
 
-df_int = pd.read_csv('amicus/debug_files/data.csv')
-df_int.pop('decision')
-
 df = pd.read_csv('amicus/scdb/SCDB_2015_01_caseCentered_Citation.csv')
-print '\n'.join(df.columns)
-
 columns_of_interest = ['docket',                       'dateArgument',                       'caseIssuesId',                       'certReason',                       'naturalCourt',                       'chief',                       'issueArea',                       'jurisdiction',                       'partyWinning']
 
 df_subset = df[df['docket'].isin(approved_cases)][columns_of_interest]
+df_scdb = df_subset[df_subset.notnull().all(axis=1)]
 
-df_subset
 
-df_subset.isnull().sum()
+# In[33]:
 
-test = df_subset[df_subset.isnull().any(axis=1)]
-df_final = df_subset[df_subset.notnull().all(axis=1)]
+df_scdb['argument_month'] = df_scdb['dateArgument'].apply(lambda d: int(d.split('/')[0]))
 
-df_final.head()
 
-df_final['argument_month'] = df_final['dateArgument'].apply(lambda d: int(d.split('/')[0]))
+# In[34]:
 
-print '\n'.join(df_final.columns)
+df_transcripts = pd.read_csv('amicus/debug_files/data.csv')
+df_transcripts.pop('decision')
 
-plt.figure(figsize=(10, 10))
-df_final['certReason'].hist(bins=13)
-plt.xticks(range(1, 15))
+df_final = pd.merge(df_scdb, df_transcripts, on='docket')
 ("")
 
-df_final['naturalCourt'].value_counts()
 
-df_final['chief'].value_counts()
+# In[35]:
 
-plt.figure(figsize=(10, 10))
-df_final['issueArea'].hist(bins=15)
-plt.xticks(range(1, 15))
-("")
+x = df_final[['argument_month']].values
+x = np.hstack((x, pd.get_dummies(df_final['certReason']).values))
+x = np.hstack((x, pd.get_dummies(df_final['naturalCourt']).values))
+x = np.hstack((x, pd.get_dummies(df_final['chief']).values))
+x = np.hstack((x, pd.get_dummies(df_final['issueArea']).values))
+x = np.hstack((x, pd.get_dummies(df_final['jurisdiction']).values))
 
-plt.figure(figsize=(10, 10))
-df_final['jurisdiction'].hist(bins=2)
-("")
-
-plt.figure(figsize=(10, 10))
-df_final['argument_month'].hist()
-("")
-
-print '\n'.join(df_final.columns)
-
-a = pd.merge(df_final, df_int, on='docket')
-
-x = a[['argument_month']].values
-x = np.hstack((x, pd.get_dummies(a['certReason']).values))
-x = np.hstack((x, pd.get_dummies(a['naturalCourt']).values))
-x = np.hstack((x, pd.get_dummies(a['chief']).values))
-x = np.hstack((x, pd.get_dummies(a['issueArea']).values))
-x = np.hstack((x, pd.get_dummies(a['jurisdiction']).values))
-
-x = np.hstack((x, pd.get_dummies(a['petitioner_interruption_count']).values))
-x = np.hstack((x, pd.get_dummies(a['respondent_interruption_count']).values))
+x = np.hstack((x, pd.get_dummies(df_final['petitioner_interruption_count']).values))
+x = np.hstack((x, pd.get_dummies(df_final['respondent_interruption_count']).values))
               
-y_true = a['partyWinning'].values
+y_true = df_final['partyWinning'].values
 
+
+# In[36]:
 
 model = LogisticRegression()
 
@@ -132,10 +71,11 @@ model = LogisticRegression()
 #print accuracy_score(y_true, y_pred)
 #print confusion_matrix(y_true, y_pred)
 
-print
-scores = cross_val_score(model, x, y_true, scoring='accuracy', cv=10)
-print scores
+scores = cross_val_score(model, x, y_true, scoring='f1', cv=10)
 print np.mean(scores)
+
+
+# In[37]:
 
 model = RandomForestClassifier()
 #model.fit(x, y_true)
@@ -147,7 +87,16 @@ model = RandomForestClassifier()
 #print accuracy_score(y_true, y_pred)
 #print confusion_matrix(y_true, y_pred)
 
-print
-scores = cross_val_score(model, x, y_true, scoring='accuracy', cv=10)
-print scores
+scores = cross_val_score(model, x, y_true, scoring='f1', cv=10)
 print np.mean(scores)
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
